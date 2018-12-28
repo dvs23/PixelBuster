9784cedb9f0d76b2c7625588a4c3f5454591510e
dvs23 <dvs23@dvs-board.de>
2018-12-28 15:48:56 +0100

    Somewhat.. better?

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <cstdint>
 #include <atomic>
+#include <thread>
+
 #include "lodepng.h"
+#include <google/sparse_hash_map>
+#include <google/sparse_hash_set>
 
 //TCP foo
 #include <stdio.h>
@@ -14,7 +18,10 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#include "concurrentqueue.h"
+using google::sparse_hash_map;
+using google::sparse_hash_set;
+
+typedef unsigned int colorNum;
 
 //hardcoded parameters
 unsigned int threadNum = 8;
@@ -24,22 +31,21 @@ std::string host = "127.0.0.1";//"151.217.40.82"
 unsigned int port = 1337;//1234
 const char hex_lookup[] = "0123456789abcdef";
 
+std::vector<std::string> colorMap;
+sparse_hash_map<std::string, colorNum> colorMapRev;
+sparse_hash_set<std::string> usedColors;
+sparse_hash_set<colorNum> invisible;
+
 std::vector<int> sockfds;
 std::vector<std::string> imgCmds;//rrggbb -> size 6
 std::vector<unsigned char> image; //the raw pixels
+std::vector<colorNum> imgCoded; //the raw pixels
+
 unsigned width, height;
 std::vector<std::string> iToStr(std::max(screenWidth, screenHeight) + 1);
 std::vector<std::string> iToHex(256);
 
-moodycamel::ConcurrentQueue<unsigned int> nextJobs;
-std::atomic<unsigned int> currIn(0);
-
-std::pair<unsigned int, unsigned int> iToXY(unsigned int i) {
-    return std::make_pair(i / width, i % width);
-}
-
-
-void sendToSocket(unsigned int tid, std::string const& msg) {
+void sendToSocket(unsigned int tid, std::string const& msg) { //unsigned int x, unsigned int y, colorNum c) {
     int ret = -1;
     unsigned int sent = 0;
 
@@ -57,7 +63,7 @@ void sendToSocket(unsigned int tid, std::string const& msg) {
     while(msg.length() - sent > 0);
 }
 
-void sendThread(unsigned int tid) {
+void sendThread(unsigned int tid, unsigned int from, unsigned int to) {
     int numbytes;
     struct hostent* he;
     struct sockaddr_in their_addr; /* connector's address information */
@@ -84,17 +90,14 @@ void sendThread(unsigned int tid) {
     }
 
     while(true) {
-        unsigned int i;
+        for(unsigned int i = from; i < to; i++)
+            sendToSocket(tid, "PX " + iToStr[i / width] + " " + iToStr[i % width] + " " + colorMap[imgCoded[i]] + "\n");
 
-        if(!nextJobs.try_dequeue(i))
-            continue;
 
-        currIn.fetch_sub(1);
-
-        if(image[i + 3] > 0)
-            sendToSocket(tid, "PX " + iToStr[i / 4 / width] + " " + iToStr[i / 4 % width] + " " + iToHex[image[i]] + iToHex[image[i + 1]] + iToHex[image[i + 2]] + iToHex[image[i + 3]] + "\n");
-
-        //std::cout << "PX " + std::to_string(i) + " " + std::to_string(j) + " 000000\n" << std::endl;
+        /*for(unsigned int i = 0; i < 1920; i++) {
+            for(unsigned int j = 0; j < 1080; j++)
+                sendToSocket(tid, "PX " + std::to_string(i) + " " + std::to_string(j) + " ffffff\n");
+        }*/
     }
 }
 
@@ -107,40 +110,43 @@ int main(int argc, char* argv[]) {
         iToHex[i] = "";
         iToHex[i] += hex_lookup[i / 16];
         iToHex[i] += hex_lookup[i % 16];
-        std::cout << i << " " << iToHex[i] << std::endl;
+        //std::cout << i << " " << iToHex[i] << std::endl;
     }
 
     sockfds = std::vector<int>(threadNum);
 
     unsigned error = lodepng::decode(image, width, height, std::string(argv[1]));
     std::cout << width << std::endl << height << std::endl;
-    std::cout << iToXY(256).first << " " << iToXY(256).second << " " << iToXY(513).first << " " << iToXY(513).second << " " << " " << std::endl << height << std::endl;
 
+    for(unsigned int i = 0; i < width * height * 4; i += 4) {
+        std::string temp = iToHex[image[i]] + iToHex[image[i + 1]] + iToHex[image[i + 2]] + iToHex[image[i + 3]];
 
-    std::thread producer([&]() {
-        while(true) {
-            for(int i = 0; i < width * height; ++i) {
-                if(currIn < width * height * 10)
-                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                else {
-                    nextJobs.enqueue(i * 4);
-                    currIn.fetch_add(1);
-                }
-            }
-
-            std::cout << currIn << std::endl;
+        if(usedColors.find(temp) == usedColors.end()) {
+            colorMap.push_back(temp);
+            colorMapRev[temp] = usedColors.size();
+            usedColors.insert(temp);
+            std::cout << temp << std::endl;
         }
-    });
 
-    std::vector<std::thread> consumers;
-
-    for(size_t i = 0; i < threadNum; i++) {
-        consumers.push_back(std::thread([&]() {
-            sendThread(i);
-        }));
+        imgCoded.push_back(colorMapRev[temp]);
     }
 
-    producer.join();
+
+
+    std::vector<std::thread> consumers;
+    //sendThread(0, 0, width * height);
+
+    for(size_t i = 0; i < threadNum - 1; i++) {
+        consumers.push_back(std::thread([&]() {
+            sendThread(i, (height * width / threadNum)*i, (height * width / threadNum) * (i + 1));
+        }));
+        std::cout << (height * width / threadNum)*i << " " << (height * width / threadNum) * (i + 1) << std::endl;
+    }
+
+    consumers.push_back(std::thread([&]() {
+        sendThread(threadNum - 1, (height * width / threadNum) * (threadNum - 1), (height * width));
+    }));
+    std::cout << (height * width / threadNum) * (threadNum - 1) << " " << height* width << std::endl;
 
     for(size_t i = 0; i < consumers.size(); i++)
         consumers[i].join();
